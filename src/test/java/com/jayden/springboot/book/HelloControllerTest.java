10e177d909631a4fda810b9db2687ac9bf4fd75a
174cm <omnia7@naver.com>
2020-08-12 23:24:15 +0900

    롬복 소개 및 설치하기 완료

--- src/test/java/com/jayden/springboot/book/HelloControllerTest.java
+++ src/test/java/com/jayden/springboot/book/HelloControllerTest.java
@@ -1,6 +1,7 @@
 package com.jayden.springboot.book;
 
 import com.jayden.springboot.book.web.HelloController;
+import com.jayden.springboot.book.web.dto.HelloResponseDto;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,9 +9,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class) /* @RunWith: 테스트를 진행할 때 JUnit에 내장된 실행자 외에 다른 실행자를 실행시킵니다.
                                           여기서는 SpringRunner라는 스프링 실행자를 사용.
@@ -27,7 +31,7 @@ public class HelloControllerTest {
                                                  이 클래스를 통해 HTTP GET, POST 등에 대한 API 테스트를 할 수 있음. */
 
     @Test
-    public void retrun_Hello() throws Exception {
+    public void return_Hello() throws Exception {
         String hello = "hello";
 
         mvc.perform(get("/hello")) /* mvc.perform(get("/hello")): MockMvc를 통해 /hello 주소로 HTTP GET 요청함
@@ -39,5 +43,26 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello)); /*.andExpect(content().string(hello)): mvc.perform의 결과를 검증함.
                                                                                             응답 본문의 내용을 검증함
                                                                                             Controller에서 "hello"를 리턴하기 때문에 이 값이 맞는지 검증함.*/
+    }
+
+    @Test
+    public void test_Lombok() throws Exception {
+        //given
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
     }
 }
